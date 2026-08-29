2d5d6dc87864bb0d36db0c9a78ddbbe2daeab3ed
eu <mauricio.00000007461@unicap.br>
2026-08-28 21:22:33 -0300

    updt

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,11 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    float speed = 5;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    public float Speed = 5;
     public Rigidbody Rb;
-    Vector3 moveVector = Vector3.zero;
+    Vector3 moveVector = Vector3.zero,
+            direction = Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
@@ -17,12 +18,20 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        direction = Vector3.zero;
 
+        if (Keyboard.current[Key.W].isPressed == true)
+        {
+            direction += transform.forward;
+        }
+        direction = Vector3.ClampMagnitude(direction, 1);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-            Rb.linearVelocity = moveVector * speed;
+        Rb.linearVelocity = new Vector3(direction.x *Speed, Rb.linearVelocity.y, direction.z *Speed);
+
+        // Rb.linearVelocity = moveVector * speed;
     }
 
     public void OnMove(InputValue value)
